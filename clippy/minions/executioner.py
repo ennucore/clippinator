52f58624f3fa860291f15223a87d13bcebfd87de
Timofey Fedoseev <fedoseev.timofey@icloud.com>
2023-04-18 21:14:22 +0200

    Add Executioner

--- clippy/minions/executioner.py
+++ clippy/minions/executioner.py
@@ -1,14 +1,19 @@
 from .base_minion import BaseMinion
 from dataclasses import dataclass
 from clippy.project import Project
+from .prompts import execution_prompt
+from clippy import tools
 
-
-@dataclass
-class Executioner(BaseMinion):
+class Executioner():
     """
     The minion responsible for executing a task.
     Can be specialized for different types of tasks (research, operations, code writing).
     """
+    execution_agent: BaseMinion
+
+    def __init__(self):
+        self.execution_agent = BaseMinion(execution_prompt, tools.get_tools())
 
     def execute(self, task: str, project: Project) -> str:
-        raise NotImplementedError
+        return self.execution_agent.run(task=task, project=project)
+    

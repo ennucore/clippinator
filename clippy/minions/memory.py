d6da3687087db98091188d8ddb85db04bc2b35b4
ennucore <ennucore@gmail.com>
2023-04-18 19:55:37 +0200

    Prompts and memory

--- clippy/minions/memory.py
+++ clippy/minions/memory.py
@@ -9,10 +9,16 @@ class Memory(BaseMinion):
     The minion responsible for:
     - Saving stuff to the memory
     - Retrieving stuff from the memory
+
+    storage: the vector storage of the memory snippets
+    available_sources: a dictionary of the available sources for the snippets, for instance, different documentations
     """
     storage: FAISS = field(default_factory=FAISS)
+    available_sources: dict[str, str] = field(default_factory=dict)
 
     def save_snippet(self, snippet: str, src: str = ''):
+        if src and src not in self.available_sources:
+            self.available_sources[src] = src
         self.storage.add_texts([snippet], [{'src': src}])
 
     def retrieve(self, query: str, n: int = 5) -> list[(str, str)]:  # (snippet, src)
--- clippy/minions/prompts.py
+++ clippy/minions/prompts.py
@@ -4,7 +4,7 @@ You are a part of a team of AI agents working on the IT project {project_name} t
 Here's some information for you: {state}
 
 You can use tools
-You need to have a "Final result:".
+You need to have a "Final Result:".
 
 You have access to the following tools:
 {tools}
@@ -24,7 +24,7 @@ execution_prompt = '''
 You are the Executor. Your goal is to execute the task in a project.''' + common_part + '''
 You need to execute the task: **{task}**.
 First, think through how you'll build the solution step-by-step. Draft the documentation for it first, then implement it (write all the necessary files etc.).
-Use the tools to do everything you need, then give the "Final result:" with the result of the task.
+Use the tools to do everything you need, then give the "Final Result:" with the result of the task.
 If there's no question in the task, give a short summary of what you did. Don't just repeat the task, include some details like filenames, function names, etc.
 If there was something unexpected, you need to include it in your result.
 
@@ -62,7 +62,7 @@ The plan (your final result) has to be in the following format:
 initial_planning = common_planning + '''
 You need to generate a plan to achieve the following objective: **{objective}**.
 Think about global things like project architecture, stack, and so on. Try to follow the TDD (test-driven development) methodology.
-Then come up with a notion (as a thought) of how it will look like in general, and then give the "Final result:" with the plan.
+Then come up with a notion (as a thought) of how it will look like in general, and then give the "Final Result:" with the plan.
 
 Begin!
 {agent_scratchpad}
@@ -80,13 +80,24 @@ Think about global things like project architecture, stack, and so on. Try to fo
 Then come up with a short notion (as a Thought) of what needs to be changed and create the plan.
 Remember that you need a full task list in the first milestone, and the tasks should be pretty simple.
 Make the first task very elaborate so that the execution agent can understand it.
-Return the complete updated plan in the "Final result:". You don't need to include the completed tasks and milestones.
+Return the complete updated plan in the "Final Result:". You don't need to include the completed tasks and milestones.
 
 Begin!
 {agent_scratchpad}
 '''
 
-memory_minion_prompt = '''
+memory_minion_prompt = 'You are the Memorizer.' + common_part + '''
+Your goal is to save information to the common brain and retrieve it from it.
+Your brain has the following kinds of information:
+{sources}
+
+You can use the AddSource tool to add a new kind of information.
+You can use the AddInfo tool to add a new piece of information to the brain.
+You can use the GetInfo tool to search the memory for some query.
+You are asked with this: {input}
+
+Begin!
+{agent_scratchpad}
 '''
 
 qa_prompt = 'You are the Tester. ' + common_part + '''
@@ -99,8 +110,8 @@ You can (and should) write tests for the task and execute the code.
 First, think of different bugs which can occur in different sections of the code. 
 If you found some bug for sure, you can reject the result and give feedback to the Executor.
 After looking for bugs, try to run the code in some way or write tests and run them.
-After that, give the "Final result:" with "ACCEPT" if everything is fine, or "REJECT" + feedback if there are some bugs.
+After that, give the "Final Result:" with "ACCEPT" if everything is fine, or "REJECT" + feedback if there are some bugs.
 After that, in the final result, you need to indicate whether the result should be accepted or rejected/improved.
-That's why your next word after "Final result:" should be either "ACCEPT" or "REJECT".
+That's why your next word after "Final Result:" should be either "ACCEPT" or "REJECT".
 After that, in the case of rejection, write the feedback for the Executor on the next line - what should be improved/fixed.
 '''

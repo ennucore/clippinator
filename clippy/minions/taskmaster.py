4b5f20edb87597291e6f21a862a35be07f23ffdb
Timofey Fedoseev <fedoseev.timofey@icloud.com>
2023-05-03 17:48:07 +0200

    Add callback

--- clippy/minions/taskmaster.py
+++ clippy/minions/taskmaster.py
@@ -15,7 +15,8 @@ from langchain.schema import BaseMemory
 from langchain.memory import ConversationSummaryMemory
 from .executioner import Executioner, get_specialized_executioners
 from .prompts import taskmaster_prompt
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Union
+from langchain.schema import AgentAction, AgentFinish, LLMResult
 
 
 class CustomMemory(BaseMemory):
@@ -74,6 +75,54 @@ class TaskmasterPromptTemplate(StringPromptTemplate):
 
 
 class CallbackHandler(BaseCallbackHandler):
+    def on_llm_start(
+        self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any
+    ) -> Any:
+        """Run when LLM starts running."""
+
+    def on_llm_new_token(self, token: str, **kwargs: Any) -> Any:
+        """Run on new LLM token. Only available when streaming is enabled."""
+
+    def on_llm_end(self, response: LLMResult, **kwargs: Any) -> Any:
+        """Run when LLM ends running."""
+
+    def on_llm_error(
+        self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
+    ) -> Any:
+        """Run when LLM errors."""
+
+    def on_chain_start(
+        self, serialized: Dict[str, Any], inputs: Dict[str, Any], **kwargs: Any
+    ) -> Any:
+        """Run when chain starts running."""
+
+    def on_chain_end(self, outputs: Dict[str, Any], **kwargs: Any) -> Any:
+        """Run when chain ends running."""
+
+    def on_chain_error(
+        self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
+    ) -> Any:
+        """Run when chain errors."""
+
+    def on_tool_start(
+        self, serialized: Dict[str, Any], input_str: str, **kwargs: Any
+    ) -> Any:
+        """Run when tool starts running."""
+
+    def on_tool_end(self, output: str, **kwargs: Any) -> Any:
+        """Run when tool ends running."""
+
+    def on_tool_error(
+        self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
+    ) -> Any:
+        """Run when tool errors."""
+
+    def on_agent_action(self, action: AgentAction, **kwargs: Any) -> Any:
+        """Run on agent action."""
+
+    def on_agent_finish(self, finish: AgentFinish, **kwargs: Any) -> Any:
+        """Run on agent end."""
+
     def on_text(self, text: str, **kwargs: Any) -> Any:
         print(text)
 
